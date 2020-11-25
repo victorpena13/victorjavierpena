d34e64bb4d067346d6e499fce66d9df9fee25ac8
victorpena13 <victor.pena1392@gmail.com>
2020-11-25 12:28:01 -0600

    GetMapping for NavBar

--- src/main/java/com/site/victorjavierpena/controller/SiteController.java
+++ src/main/java/com/site/victorjavierpena/controller/SiteController.java
@@ -5,8 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SiteController {
+
         @GetMapping(value={"/"})
         public String home(){
             return "pages/homePage";
+        }
+
+        @GetMapping(value={"/resume"})
+        public String resume() {
+            return "pages/resume";
+        }
+
+        @GetMapping(value={"/projects"})
+        public String project() {
+            return "pages/projects";
         }
 }
